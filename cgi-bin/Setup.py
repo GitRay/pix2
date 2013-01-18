b8003e9f41b6ac6dd2d1ddae1178fbec43bbdbea
RLC <github@cathcart.us>
2013-01-17 22:01:47 -0500

    * Changed setup paths so that they distinguish between the directory structure as seen by the OS and as seen by the browser

--- cgi-bin/Setup.py
+++ cgi-bin/Setup.py
@@ -3,30 +3,34 @@
 ## this particular file is released into the public domain by its
 ## sole author, Ray Cathcart.
 
-import os, sys
-
 seperator  = '&raquo;'
 pixVersion = '1.4.0'
 
 # Absolute path to the CGI directory on your webserver
-pathToCGI = os.path.abspath(os.path.dirname(sys.argv[0]))
+#pathToCGI = os.path.abspath(os.path.dirname(sys.argv[0]))
+pathToCGI = '/usr/local/www/apache22/cgi-bin'
+webPathToCGI = '/cgi-bin'
 
 # Absolute path to the photo album on the webserver
-albumLoc = os.path.join(pathToCGI,'..','test_album')
+albumLoc = '/album_loc'
 
 # Absolute path to the other web resources (including template.html)
-pathToTemplate = os.path.join(pathToCGI,'..')
+pathToTemplate = '/usr/local/www/apache22/data'
+webPathToTemplate = '/'
 
-# Absolute path to the web root - the base web server directory
-pathToWebRoot = os.path.join(pathToCGI,'..')
+# Path that appears in url when at your webserver's root location
+pathToWebRoot = '/'
 
 # Absolute path to the picture cache folder
-pathToPicCache = os.path.join(pathToCGI,'..','pic_cache')
+pathToPicCache = '/usr/local/www/apache22/data/pic_cache'
+webPathToPicCache = '/pic_cache'
 
 # if this is false we try to use ImageMagick
 # if you set it to true we try to import and use PIL
 USE_PIL = True
 
 # Path to the imageMagick "identify" and "convert" utilities
-pathToIdentify = '/opt/local/bin/identify'
-pathToConvert = '/opt/local/bin/convert'
+#pathToIdentify = '/opt/local/bin/identify' # Mac using ports
+#pathToConvert = '/opt/local/bin/convert'
+pathToIdentify = '/usr/local/bin/identify'  # FreeBSD using ports
+pathToConvert = '/usr/local/bin/convert'
